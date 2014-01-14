80cccfc731468ca8df90cb0e8edd3bae78592d70
kurttheviking <kurt@agilediagnosis.com>
2014-01-14 09:55:41 -0800

    initial support for list processing

--- index.js
+++ index.js
@@ -1,44 +1,81 @@
-var plumb = require('plumb')
-var decode = require('he').decode
+var cheerio = require('cheerio');
+var decode = require('he').decode;
+var plumb = require('plumb');
 
-var html2plaintext = plumb(
+
+// "private" helper for list processing into plaintext
+function _list (str, isOrdered) {
+  if (!str) return str;
+
+  var $ = cheerio.load(str);
+  var listEl = isOrdered ? 'ol' : 'ul';
+
+  $(listEl).each(function (i, el) {
+    var $out = cheerio.load('<p></p>');
+    var $el = $(el);
+
+    $el.find('li').each(function (j, li) {
+      var tick = isOrdered ? String(j + 1) + '.' : '-';
+
+      $out('p').append(tick + ' ' + $(li).text() + '<br />');
+    });
+
+    // avoid excess spacing coming off last element
+    // (we are wrapping with a <p> anyway)
+    $out('br').last().remove();
+
+    $el.replaceWith($out.html());
+  });
+
+  return $.html();
+}
+
+function stringify(x) {
+  return x ? x.toString() : '';
+}
+
+function collapseWhitespace (val) {
+  val = val.replace(/\s+/g, ' ');
+
+  return val;
+}
+
+function linebreaks (str) {
+  return str.replace(/<\s?(p|br)[^<]*>/gi, function (x, tag) {
+    switch (tag.toLowerCase()) {
+      case 'p':
+        return '\n\n';
+      case 'br':
+        return '\n';
+    }
+
+    return x;
+  });
+}
+
+function listOrdered (str) {
+  return _list(str, true);
+}
+
+function listUnordered (str) {
+  return _list(str, false);
+}
+
+function stripTags (str) {
+  return str.replace(/<[^<]+>/g, '');
+}
+
+function trim (str) {
+  return str.trim();
+}
+
+module.exports = plumb(
+  listOrdered,
+  listUnordered,
   stringify,
   collapseWhitespace,
   linebreaks,
   stripTags,
   decode,
   trim
-)
-
-
-function stringify(x) {
-  return x ? x.toString()
-           : ''
-}
-
-function collapseWhitespace (val) {
-  val = val.replace(/\s+/g, ' ')
-  return val
-}
-
-function linebreaks (str) {
-  return str.replace(/<\s?(p|br)[^<]*>/gi, function (x, tag) {
-    switch(tag.toLowerCase()){
-      case 'p':
-        return '\n\n'
-      case 'br':
-        return '\n'
-    }
-    return x
-  })
-}
-
-function stripTags (str) {
-  return str.replace(/<[^<]+>/g, '')
-}
-
-function trim (str) {
-  return str.trim()
-}
-
-module.exports = html2plaintext
+);
--- test/test.js
+++ test/test.js
@@ -4,54 +4,75 @@ var heredoc = require('heredoc')
 var h2p = require('../index')
 
 describe('html2plaintext', function () {
+
   it('strips html tags', function () {
-
     h2p('<b>HELLO <blink>world</blink></b>')
-      .should.equal('HELLO world')
+      .should.equal('HELLO world');
+  });
 
-  })
   it('treats whitespace like a browser - trim', function () {
     h2p('<b>Hey there, Jon</b>  \n')
-      .should.equal('Hey there, Jon')
-  })
+      .should.equal('Hey there, Jon');
+  });
+
+  it('parses ordered lists into enumerated lines', function () {
+    h2p('<ol><li>First there is this</li><li>Then there is that</li></ol>')
+      .should.equal('1. First there is this\n2. Then there is that');
+  });
+
+  it('parses unordered lists into ticked lines', function () {
+    h2p('<ul><li>First there is this</li><li>Then there is that</li></ul>')
+      .should.equal('- First there is this\n- Then there is that');
+  });
+
   it('treats whitespace like a browser - collapse repeated space', function () {
     h2p('<h1>It is  aa maaaa   zzzzz       ing!</h1>')
-      .should.equal('It is aa maaaa zzzzz ing!')
-  })
+      .should.equal('It is aa maaaa zzzzz ing!');
+  });
+
   it('strips html whitespace entities', function () {
     h2p('hi&nbsp;')
-      .should.equal('hi')
-  })
+      .should.equal('hi');
+  });
+
   it('decodes all kinds of HTML entities', function () {
     h2p('Foo &copy; bar &#x1D306; baz &#9731; qux &awint;')
-      .should.equal('Foo \xA9 bar \uD834\uDF06 baz \u2603 qux \u2A11')
-  })
+      .should.equal('Foo \xA9 bar \uD834\uDF06 baz \u2603 qux \u2A11');
+  });
+
   it('ignores linebreaks', function () {
     h2p('hi\n\n\n\n<b>foo</b>\n')
-      .should.equal('hi foo')
-  })
+      .should.equal('hi foo');
+  });
+
   it('ignores single linebreaks', function () {
     h2p('hi\nii')
-      .should.equal('hi ii')
-  })
+      .should.equal('hi ii');
+  });
+
   it('inserts 2 linebreaks after </p>', function () {
     h2p('<p>it was the best of times</p><p>it was the worst of times</p>')
-      .should.equal('it was the best of times\n\nit was the worst of times')
-  })
+      .should.equal('it was the best of times\n\nit was the worst of times');
+  });
+
   it('inserts 1 linebreak <br>', function () {
     h2p('hello<br/>goodbye')
-      .should.equal('hello\ngoodbye')
-  })
+      .should.equal('hello\ngoodbye');
+  });
+
   it('decodes html entities', function () {
     h2p('&lt;3 &amp; &lt;/3')
-      .should.equal('<3 & </3')
-  })
+      .should.equal('<3 & </3');
+  });
+
   it('calls toString on input', function () {
     h2p(3290782432)
-      .should.equal('3290782432')
-  })
+      .should.equal('3290782432');
+  });
+
   it('is safe on null or undefined input', function () {
-    h2p(null).should.equal('')
-    h2p().should.equal('')
-  })
-})
+    h2p(null).should.equal('');
+    h2p().should.equal('');
+  });
+
+});
